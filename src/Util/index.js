240ddd3da5a8307b39ef730826d291fd1febd9c0
Hany El Nokaly <hany.elnokaly@gmail.com>
2018-06-20 03:55:15 +0200

    add random integer function to util

--- src/Util/index.js
+++ src/Util/index.js
@@ -25,4 +25,15 @@ async function valueOf (value) {
   return value
 }
 
-module.exports = {serialize, deserialize, valueOf}
+/**
+ * Returns integer number between two numbers (inclusive)
+ * 
+ * @param {int} min 
+ * @param {int} max 
+ * @return int
+ */
+function randomIntBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+module.exports = {serialize, deserialize, valueOf, randomIntBetween}
